3ca1a88dd54c53e9970205ef51045271f7f03b74
TheAvtarSingh <singha2k2@gmail.com>
2023-06-03 22:32:20 +0530

    cors;

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 // Middleware
 app.use(express.json());
 
